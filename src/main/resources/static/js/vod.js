92d06e02a5f8cff0c4679f4d316983b8fe1ed424
Administrator <Administrator@HY0473-PC>
2017-05-10 18:08:29 +0800

    初步完成 js 点播 列表重复检测逻辑

--- src/main/resources/static/js/vod.js
+++ src/main/resources/static/js/vod.js
@@ -1,106 +1,157 @@
-var intInterval = 0;
-var indexFile;
 $(document).ready(function() {
-		var successVideoList = [];
-		var nowIndex = 0;
-		var hasEvent = false;
-		var firstOk = false;
-		function findVideoByFileNameInCache(fileName){
-			var video ;
-			$.each(successVideoList,function(key,val){
-				if(this.fileName == fileName ){
-					video = this;
-					return false;
+	var intInterval = 0;
+	var localVideoList = (function(){
+		var videoList = [];
+		var videoEleId = "example-video";//真正视频标签
+		var applyVideoType = "video/mp4";//视频格式
+		var tmpVideoEleId = "tmpVideo";//临时视频
+		var videoIdTag = $('#hidid');
+		var myPlayer;
+		var playIndex = -1;
+		var indexFile;
+		function initVideoEle(){
+			myPlayer = videojs(videoEleId); //初始化视频
+			$('#'+videoEleId).addClass('video-js');
+			initEvent();//事件初始化
+		}
+		function initEvent(){
+			//视频播放停止事件
+			myPlayer.on("ended",function(){
+	    		var video = videoList[playIndex];
+	    		if(!!video){
+	    			playVedio(videoIdTag.val(),video.fileName)
+	    		}
+	    		//通知客户端 已经看完了多少视频 由服务器 去做处理
+	    	});
+			//视频播放进度事件
+	    	myPlayer.on("timeupdate",function(){
+	    		var video = videoList[playIndex];
+	    		if(!!indexFile && this.currentTime() > (indexFile.splitSecStep*0.6) && !!video && !video.loaded){//视频缓存 让视频切换不卡	
+	    			video.loaded = true;//相当于自锁
+	    			cacheVideo(video);
+	    		}
+	    	})
+		}
+		//通过视频的视频找到视频
+		function findVideoInCache(video){
+			//先排序 
+			updateVideoSort("asc");
+			//
+			for(var i=0;i<videoList.length;i++){
+				if(videoList[i].fileName == video.fileName ){
+					newVideo = videoList[i];
+					return newVideo;
 				}
-			});
-			return video;
+			}
+			
+			return null;
+		}
+		//添加一个视频列表单元
+		function saveVideoItem(video){
+			var oldVideo = findVideoInCache(video);
+			if(!!!oldVideo)
+				videoList.push(video);
+			else{
+				oldVideo.downLoadState = video.downLoadState;//更新客户端视频状态
+			}
+			return oldVideo;
+		}
+		//更新视频列表排序
+		function updateVideoSort(direct){
+			if(videoList.length > 0){
+				videoList.sort(function(v1,v2){
+					if(direct == "asc")
+						return new Date(v1.startTime).getTime() -  new Date(v2.startTime).getTime();
+					else
+						return new Date(v2.startTime).getTime() -  new Date(v1.startTime).getTime();
+				});
+			}
+		}
+		//缓存视频
+		function cacheVideo(video){
+			var tmpVideo = document.querySelector("#"+tmpVideoEleId);
+			if(!tmpVideo){
+				var videoTag = $("<video  id='"+tmpVideoEleId+"'src='"+"/"+videoIdTag.val()+"/"+video.fileName+"'></video>");
+				videoTag.hide();
+				$("body").append(videoTag);
+			}else{
+				tmpVideo.src="/"+videoIdTag.val()+"/"+video.fileName;
+				tmpVideo.load();
+			}
 		}
 		//视频播放
 		function playVedio(id, filename) {
-			$('#example-video').addClass('video-js');
-            $('#hidid').val(id);
-            //播放视频
-            var myPlayer = videojs("example-video");  //初始化视频
-            var u = '/' + id + '/' + filename;
-            //myPlayer.src([{ src: u, type: 'video/flv' }]);
-            myPlayer.src([{ src: u, type: 'video/mp4' }]);
-            //myPlayer.load(u);  //使video重新加载
-            myPlayer.play();
-            nowIndex++;
-            if(!hasEvent){//检测播放进度
-            	hasPauseEvent = true;
-            	myPlayer.on("ended",function(){
-            		var video = successVideoList[nowIndex];
-            		if(!!video){
-            			playVedio($('#hidid').val(),video.fileName)
-            		}
-            		//通知客户端 已经看完了多少视频 由服务器 去做处理
-            	});
-            	myPlayer.on("timeupdate",function(){
-            		var video = successVideoList[nowIndex];
-            		if(!!indexFile && this.currentTime() > (indexFile.splitSecStep*0.6) && !!video && !video.loaded){//视频缓存 让视频切换不卡	
-            			video.loaded = true;
-            			var videoId = "tmpVideo";
-            			var tmpVideo = document.querySelector("#"+videoId);
-            			if(!tmpVideo){
-            				var videoTag = $("<video  id='"+videoId+"'src='"+"/"+$('#hidid').val()+"/"+video.fileName+"'></video>");
-            				videoTag.hide();
-            				$("body").append(videoTag);
-            				console.log("上次播放："+successVideoList[nowIndex - 1].startTime,"需要提前缓存:"+video.startTime)
-            			}else{
-            				tmpVideo.src="/"+$('#hidid').val()+"/"+video.fileName;
-            				tmpVideo.load();
-            				console.log("上次播放："+successVideoList[nowIndex - 1].startTime,"需要提前缓存:"+video.startTime)
-            			}	
-            		}
-            	})
-            }
+	        //播放视频
+			if(!!myPlayer)
+				initVideoEle()
+	        var u = '/' + id + '/' + filename;
+	        myPlayer.src([{ src: u, type: applyVideoType }]);
+	        myPlayer.play();
+	  
 		}
-		// 计算已经下载的文件数
-		window.refreshIndexFile = function(monitorId) {
+		function getVideoList(){
+			updateVideoSort("asc");
+			return videoList;
+		}
+		function refreshIndexFile() {
 			$.ajax({
 				type : 'get',
 				async : false,
-				url : "/" + monitorId + "/index.json",
+				url : "/" + videoIdTag.val() + "/index.json",
 				cache : false,
 				success : function(data) {
 					indexFile = data;
 					var videoList = data.videos;
 					successedMaxNum = data.successedNum;
+					var newVideoList = [];
 					for (var i = 0; i < videoList.length; i++) {
 						var video = videoList[i];
-						if(video.downLoadState =="已经转码" && !findVideoByFileNameInCache(video.fileName)){
-							successVideoList.push(video);
-							if(successVideoList[0] && !firstOk){
-								firstOk = true;
-								playVedio($('#hidid').val(),video.fileName);
-							}
+						var clientStartTime = new Date($('#startTime').val());
+						var clientEndTime = new Date($('#endTime').val());
+						if(
+								video.vodReqState =="已经请求" //切记这个状态 因为视频列表是用户共享的
+								&& 
+								clientStartTime.getTime() <= new Date(video.startTime).getTime()
+								&&//只拿客户点播的视频时间段内视频列表
+								clientEndTime.getTime() >= new Date(video.endTime).getTime())
+						{
+							localVideoList.saveVideoItem(video);
 						}
 					}
 				}
 			});
 		}
-		$("#goBtn").on("click",function() {
-			var param = {
-				monitorId : $('#hidid').val(),
-				startTime : $('#startTime').val(),
-				endTime : $("#endTime").val()
-			};
-			// 首先开启播放
-			$.ajax({
-				type : 'post',
-				async : false,
-				url : "/monitor/publish_vod",
-				data : param,
-				// dataType: 'jsonp',
-				cache : false,
-				success : function(data) {
-					var monitorId = data.parentPathName;
-					intInterval = window.setInterval("refreshIndexFile('" + monitorId + "')", 1000);
-				}
-			});
-			
+		return {
+			playVedio:playVedio,
+			saveVideoItem:saveVideoItem,
+			findVideoInCache:findVideoInCache,
+			getVideoList:getVideoList,
+			refreshIndexFile:refreshIndexFile
+		}
+		
+	})();
 
+	window.localVideoList = localVideoList;
+	//视频播放
+	
+	$("#goBtn").on("click",function() {
+		var param = {
+			monitorId : $('#hidid').val(),
+			startTime : $('#startTime').val(),
+			endTime : $("#endTime").val()
+		};
+		// 首先开启播放
+		$.ajax({
+			type : 'post',
+			async : false,
+			url : "/monitor/publish_vod",
+			data : param,
+			// dataType: 'jsonp',
+			cache : false,
+			success : function(data) {
+				if(!intInterval)
+					intInterval = window.setInterval(localVideoList.refreshIndexFile, 1000);
+			}
 		});
-	}
-);
+	});
+});
